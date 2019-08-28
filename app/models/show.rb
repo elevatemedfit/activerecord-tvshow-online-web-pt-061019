95c384afcc86d8e89ba7c6d191cd72ec55ad0d47
Chris Junker <elevatemedfit@gmail.com>
2019-08-28 14:40:41 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def Show::most_popular_show
-    Show.order(rating: :desc).distinct
+    Show.order(rating: :desc).first
   end
 
   def Show::lowest_rating

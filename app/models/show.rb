1e0a75094b089a16b89609059306d5c673c49020
Chris Junker <elevatemedfit@gmail.com>
2019-08-28 14:18:08 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -19,4 +19,8 @@ class Show < ActiveRecord::Base
   def Show::popular_shows
     Show.where("rating > ?", 5)
   end
+
+  def Show::shows_by_alphabetical_order
+    Show.order(name: :asc)
+  end
 end
